21818fdde7e3d131b43cdc12d15007d7a5151730
Jonathan Sarmiento <jhonn.photo@gmail.com>
2022-07-15 13:27:56 -0500

    feat: Add toogle class to password visibility button

--- client/src/components/Signup/component.jsx
+++ client/src/components/Signup/component.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {AiOutlineEye,AiOutlineEyeInvisible} from "react-icons/ai"
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import axios from "axios";
 import "./style.css";
 import { useNavigate } from "react-router-dom";
@@ -9,25 +9,29 @@ const Signup = () => {
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
   const [username, setUsername] = useState("");
   const [address, setAddress] = useState("");
+  const [eyeActive, seteyeActive] = useState(true);
   const navigate = useNavigate();
   let handleSubmit = async (e) => {
     e.preventDefault();
-      const data = {
-        email: email,
-        password: password,
-        password2: passwordConfirmation,
-        username: username,
-        address: address,
-      };
-      try {
-        const res = await axios.post("http://192.168.0.102:3000/users/signup",data);
-        if (res.data.success === true) {
-          alert("User Created, Redirecting to Login Page...");
-          navigate("/login");
-        } else alert(res.data.reason);
-      } catch (error) {
-        console.log(error);
-      }
+    const data = {
+      email: email,
+      password: password,
+      password2: passwordConfirmation,
+      username: username,
+      address: address,
+    };
+    try {
+      const res = await axios.post(
+        "http://192.168.0.102:3000/users/signup",
+        data
+      );
+      if (res.data.success === true) {
+        alert("User Created, Redirecting to Login Page...");
+        navigate("/login");
+      } else alert(res.data.reason);
+    } catch (error) {
+      console.log(error);
+    }
   };
   const getUser = async () => {
     const user = JSON.parse(localStorage.getItem("user"));
@@ -39,6 +43,10 @@ const Signup = () => {
   useEffect(() => {
     getUser();
   }, []);
+
+  const handleToggle = () => {
+    seteyeActive(!eyeActive);
+  };
 
   return (
     <>
@@ -67,14 +75,28 @@ const Signup = () => {
               value={address}
               onChange={(e) => setAddress(e.target.value)}
             />
-             <input
+            <input
               type="text"
               className="signup__formContainer-input"
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+            />
+            {eyeActive ? (
+              <AiOutlineEye
+                onClick={handleToggle}
+                size="20px"
+                color="#808080"
+                className="signup__formContainer-fa-eye"
               />
-              <AiOutlineEye size="20px" color="#808080" className="signup__formContainer-fa-eye"/>
+            ) : (
+              <AiOutlineEyeInvisible
+                onClick={handleToggle}
+                size="20px"
+                color="#808080"
+                className="signup__formContainer-fa-eye"
+              />
+            )}
             <input
               type="text"
               className="signup__formContainer-input"
@@ -82,7 +104,21 @@ const Signup = () => {
               value={passwordConfirmation}
               onChange={(e) => setPasswordConfirmation(e.target.value)}
             />
-            <AiOutlineEye size="20px" color="#808080" className="signup__formContainer-fa-eye fa-eye-confirm"/>
+             {eyeActive ? (
+              <AiOutlineEye
+                onClick={handleToggle}
+                size="20px"
+                color="#808080"
+                className="signup__formContainer-fa-eye fa-eye-confirm"
+              />
+            ) : (
+              <AiOutlineEyeInvisible
+                onClick={handleToggle}
+                size="20px"
+                color="#808080"
+                className="signup__formContainer-fa-eye fa-eye-confirm"
+              />
+            )}
             <button type="submit" className="signup__form-btn" value="submit">
               Signup
             </button>
