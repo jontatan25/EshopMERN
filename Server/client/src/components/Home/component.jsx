8212cc6ca52c3bcd2525c579d55d2f532a86b748
Jonathan Sarmiento <jhonn.photo@gmail.com>
2022-10-10 05:36:03 -0500

    feat: Homepage updated:
    new UI implemented. Offer carousel sign With button completed

--- Server/client/src/components/Home/component.jsx
+++ Server/client/src/components/Home/component.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import "./style.css";
+import carrouselBackground from "../../images/carrousel/carrousel-background.jpg";
+import carouselSign from "../../../src/images/carrousel/summerSale.svg";
 
 const HomeContainer = () => {
-
-  return <div className="homeContainer">HomePage</div>;
+  return (
+    <div
+      className="homeContainer"
+      style={{ backgroundImage: `url(${carrouselBackground})` }}
+    >
+      <div className="carousel__container">
+        <div className="carousel__sign">
+          <div className="carousel__sign-text-container">
+            <img className= "carousel__sign-text" src={carouselSign} alt="" />
+          </div>
+          <button className="carousel__sign-btn">SHOP NOW</button>
+        </div>
+        <div className="carousel__images"></div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeContainer;
