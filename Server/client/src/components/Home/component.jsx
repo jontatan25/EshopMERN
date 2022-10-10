61ce06e2ba1648db7eb37554ce31c482eaaa2349
Jonathan Sarmiento <jhonn.photo@gmail.com>
2022-10-10 07:18:55 -0500

    feat: carousel images implemented with shadow

--- Server/client/src/components/Home/component.jsx
+++ Server/client/src/components/Home/component.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./style.css";
 import carrouselBackground from "../../images/carrousel/carrousel-background.jpg";
 import carouselSign from "../../../src/images/carrousel/summerSale.svg";
+import smallImage from "../../images/carrousel/smallImg.jpg";
+import bigImage from "../../images/carrousel/bigImage.jpg";
 
 const HomeContainer = () => {
   return (
@@ -12,11 +14,20 @@ const HomeContainer = () => {
       <div className="carousel__container">
         <div className="carousel__sign">
           <div className="carousel__sign-text-container">
-            <img className= "carousel__sign-text" src={carouselSign} alt="" />
+            <img className="carousel__sign-text" src={carouselSign} alt="" />
           </div>
           <button className="carousel__sign-btn">SHOP NOW</button>
         </div>
-        <div className="carousel__images"></div>
+        <div className="carousel__images">
+          <div
+            className="carousel__images-smallImage"
+            style={{ backgroundImage: `url(${smallImage})` }}
+          ></div>
+          <div
+            className="carousel__images-bigImage"
+            style={{ backgroundImage: `url(${bigImage})` }}
+          ></div>
+        </div>
       </div>
     </div>
   );
