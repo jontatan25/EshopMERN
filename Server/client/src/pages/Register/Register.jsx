65bd0c3b9faeed491818594095dd20dd428f6b99
Jonathan Sarmiento <jhonn.photo@gmail.com>
2022-11-05 11:24:58 -0500

    /Client -Feat: Alerts Implemented with Register

--- Server/client/src/pages/Register/Register.jsx
+++ Server/client/src/pages/Register/Register.jsx
@@ -1,25 +1,28 @@
 import React, { useState } from "react";
 import "./style.css";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+
+const API_ENDPOINT =
+  process.env.REACT_APP_API_ENDPOINT ||
+  "https://mern-eshop-espitia-jonathans.herokuapp.com";
 
 const Register = () => {
-
-  // const [email, setEmail] = useState(null);
-  // const [password, setPassword] = useState(null);
-  // const [confirmPassword, setConfirmPassword] = useState(null);
-  // const [username, setUsername] = useState(null);
-  // const [address, setAddress] = useState(null);
-
-  const [info,setInfo] = useState([{
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    address: "",
-  }]);
-
+  const [info, setInfo] = useState([
+    {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      address: "",
+    },
+  ]);
+  const navigate = useNavigate();
   const [eyeActive, seteyeActive] = useState(false);
   const [confirmationActive, setConfirmationActive] = useState(false);
+  const [wrongPasswordStyle, setwrongPasswordStyle] = useState(false);
 
   const handleTogglePassword = () => {
     seteyeActive(!eyeActive);
@@ -30,33 +33,114 @@ const Register = () => {
   };
 
   const handleFormChange = (e) => {
-    let updatedInfo = [...info]
+    let updatedInfo = [...info];
     updatedInfo[0][e.target.name] = e.target.value;
     setInfo(updatedInfo);
-    console.log(updatedInfo)
-  }
+  };
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    const data = info[0];
+    if (data.password === data.confirmPassword) {
+      Swal.fire({
+        title: "Creating your account, please wait...",
+        didOpen: () => {
+          Swal.showLoading();
+        },
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false,
+      });
+      async function createAccount() {
+        try {
+          const res = await axios.post(`${API_ENDPOINT}/users/signup`, data);
+          if (res.data.success === true) {
+            Swal.close();
+            Swal.fire({icon: "success", title: "Account Created", text:"You can now Sign in!"})
+            .then(() => {
+              Swal.close();
+              Swal.fire({
+                title: 'You are being redirected...',
+                text: "Please wait ...",
+                showConfirmButton: false,
+                timer: 3000,
+              })
+              .then(() => navigate("/"));
+            }
+            )
+            
+            
+          } else {
+            const rejectReason = res.data.reason
+            Swal.close();
+            Swal.fire({
+              icon: 'error',
+              title: 'ERROR',
+              text: `${rejectReason}`,
+              footer: 'Please try again'
+            })
+          }
+        } catch (error) {
+          console.log(error);
+          Swal.close();
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+            footer: '<a href="">Why do I have this issue?</a>'
+          })
+        }
+      }
+      createAccount();
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Passwords Does not match!",
+        footer: "Please try again.",
+      });
+      setwrongPasswordStyle(true);
+    }
+  };
+
   return (
     <>
       <h5 className="register__url">Home / Register</h5>
       <h3 className="register__title">Create a New Account</h3>
       <div className="register__container">
         <div className="register__info-container">
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <h4 className="register__form-title">Personal Information</h4>
             <div className="register__field">
               <label htmlFor="username" className="register__lb">
                 Username <span>*</span>
               </label>
-              <input type="text" className="register__in" id="username" name="username" onChange={e=> handleFormChange(e)} value={info.username}/>
+              <input
+                type="text"
+                className="register__in"
+                id="username"
+                name="username"
+                onChange={(e) => handleFormChange(e)}
+                value={info.username}
+                required
+              />
             </div>
             <div className="register__field">
               <label htmlFor="email" className="register__lb">
                 Email <span>*</span>
               </label>
-              <input type="email" className="register__in" id="email" name="email" onChange={e=> handleFormChange(e)} value={info.email}/>
+              <input
+                type="email"
+                className="register__in"
+                id="email"
+                name="email"
+                onChange={(e) => handleFormChange(e)}
+                value={info.email}
+                required
+              />
             </div>
             <div className="register__field">
-              <label htmlFor="password" className="register__lb" >
+              <label htmlFor="password" className="register__lb">
                 Password <span>*</span>
               </label>
               <div className="register__vision-container">
@@ -65,7 +149,9 @@ const Register = () => {
                   className="register__in"
                   id="password"
                   name="password"
-                  onChange={e=> handleFormChange(e)} value={info.password}
+                  onChange={(e) => handleFormChange(e)}
+                  value={info.password}
+                  required
                 />
                 {eyeActive ? (
                   <AiOutlineEye
@@ -88,13 +174,21 @@ const Register = () => {
               <label htmlFor="confirm-password" className="register__lb">
                 Confirm Password <span>*</span>
               </label>
-              <div className="register__vision-container">
+              <div
+                className={
+                  wrongPasswordStyle === false
+                    ? "register__vision-container"
+                    : "register__vision-container register__vision-container-red"
+                }
+              >
                 <input
                   type={confirmationActive ? "text" : "password"}
                   className="register__in"
                   id="confirm-password"
                   name="confirmPassword"
-                  onChange={e=> handleFormChange(e)} value={info.confirmPassword}
+                  onChange={(e) => handleFormChange(e)}
+                  value={info.confirmPassword}
+                  required
                 />
                 {confirmationActive ? (
                   <AiOutlineEye
@@ -117,7 +211,15 @@ const Register = () => {
               <label htmlFor="address" className="register__lb">
                 Address <span>*</span>
               </label>
-              <input type="text" className="register__in" id="address" name="address" onChange={e=> handleFormChange(e)} value={info.address}/>
+              <input
+                type="text"
+                className="register__in"
+                id="address"
+                name="address"
+                onChange={(e) => handleFormChange(e)}
+                value={info.address}
+                required
+              />
             </div>
             <div className="register__btn-container">
               <button type="submit" className="register__btn-create">
