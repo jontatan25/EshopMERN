3be618e859e904dd6e6974171b0b100d9c8fe047
Jonathan Sarmiento <jhonn.photo@gmail.com>
2022-11-11 06:49:45 -0500

    /Client -Feat: Burguer Menu implemented.

--- Server/client/src/components/NavigationBar/component.jsx
+++ Server/client/src/components/NavigationBar/component.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink,Link } from "react-router-dom";
 import "./style.css";
 import logo from "../../images/logo/logo.svg";
 import { useCartContext } from "../CartContext/context";
@@ -7,6 +7,8 @@ import { useCartContext } from "../CartContext/context";
 import Swal from "sweetalert2";
 
 const NavigationBar = () => {
+
+  const [showMenu,setShowMenu] = useState(false)
   const { showLogin, setShowLogin, cart, loggedIn, setLoggedIn } =
     useCartContext();
 
@@ -43,6 +45,9 @@ const NavigationBar = () => {
       }
     });
   };
+ const toogleMenu = () => {
+   setShowMenu(!showMenu)
+ }
   return (
     <>
       <div className="header__Container">
@@ -172,9 +177,48 @@ const NavigationBar = () => {
       <div className="header__Container-responsive">
         <nav className="nav">
           <div className="nav__item">
-            <button className="nav__burguer">
+            <button className={!showMenu ? "nav__burguer":"nav__burguer burguer-active" } onClick={() => toogleMenu()}>
               <span></span>
             </button>
+          </div>
+          <div className={!showMenu ? "nav__menu" : "nav__menu nav__active"}>
+            <ul className="nav__list">
+              <li className="nav__list-listItem">
+                <Link to="/">
+                  <button className="listItem__btn">
+                    <span>HOME</span>
+                  </button>
+                </Link>
+              </li>
+              <li className="nav__list-listItem">
+                <Link to="/products">
+                  <button className="listItem__btn">
+                    <span>SHOP</span>
+                  </button>
+                </Link>
+              </li>
+              <li className="nav__list-listItem">
+              <Link to="/blog">
+                  <button className="listItem__btn">
+                    <span>BLOG</span>
+                  </button>
+                </Link>
+              </li>
+              <li className="nav__list-listItem">
+              <Link to="/cart">
+                  <button className="listItem__btn">
+                    <span>MY BAG</span>
+                  </button>
+                </Link>
+              </li>
+              <li className="nav__list-listItem">
+              <Link to="/wishlist">
+                  <button className="listItem__btn">
+                    <span>MY WISHLIST</span>
+                  </button>
+                </Link>
+              </li>
+            </ul>
           </div>
           <div className="nav__item nav__logo">
             <NavLink to="/">
